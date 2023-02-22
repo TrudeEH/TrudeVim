81d927e05cd7a19676c7ab61008480233735f671
TrudeEH <ehtrude@gmail.com>
2023-02-22 19:36:28 +0000

    Add LSP and bug fixes

--- init.lua
+++ init.lua
@@ -52,7 +52,7 @@ require('lazy').setup({
   'kyazdani42/nvim-tree.lua', -- Custom File Explorer
 
   'akinsho/bufferline.nvim', -- Top bar for buffers (Buffer = file loaded in memory)
-  'moll/vim-bbye', -- ??????????????????????
+  'moll/vim-bbye', -- Bdelete command
 
   'akinsho/toggleterm.nvim', -- Toggle a terminal
 
@@ -107,7 +107,12 @@ require('lazy').setup({
           project = { enable = false, },
           footer = {},
           shortcut = {
-            { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+            { 
+              desc = ' Update', 
+              group = '@property', 
+              action = 'Lazy update', 
+              key = 'u' 
+            },
             {
               icon = ' ',
               icon_hl = '@variable',
@@ -122,7 +127,15 @@ require('lazy').setup({
               desc = 'Browse Keymaps',
               group = 'Label',
               action = 'Telescope keymaps',
-              key = 'k',
+              key = 'i',
+            },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Language Servers',
+              group = 'Label',
+              action = 'Mason',
+              key = 'l',
             },
           },
         },
@@ -280,6 +293,7 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "NvimTree" })
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<cr>", { desc = "Telescope Find Files" })
 vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Telescope Live Grep" })
 vim.keymap.set("n", "<leader>q", "<cmd>Bdelete<cr>", { desc = "Close Selected Buffer" })
+vim.keymap.set("n", "<leader>d", "<cmd>Dashboard<cr>", { desc = "Dashboard" })
 
 -- ==================================================================== --
 --                          Plugin Configuration                        --
@@ -413,93 +427,60 @@ require('nvim-treesitter.configs').setup {
 --         Langauge Server - Error checking / Custom lang features      --
 -- ==================================================================== --
 
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+-- Install lang servers:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
-  -- Create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
+local servers = {
+  "lua_ls",
+  "bashls",
+  "cssls",
+  "html",
+  "tsserver",
+}
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+local settings = {
+	max_concurrent_installers = 2,
+  ui = {
+    icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
+    }
+  }
+}
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+require("mason").setup(settings)
+require("mason-lspconfig").setup({
+	ensure_installed = servers,
+	automatic_installation = true,
+})
 
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+local on_attach = function(_, _)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename all definitions"})
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, { desc = "Code Actions" })
 
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Jump to the definition of the symbol under the cursor" })
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Implementation" })
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = "References of functions" })
+  vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, { desc = "Documentation" })
 end
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+for _, server in ipairs(servers) do
+  require("lspconfig")[server].setup({
+    on_attach = on_attach,
+  })
+end
 
-  lua_ls = {
+require("lspconfig").lua_ls.setup{
+  settings = {
     Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
-}
-
--- Setup neovim lua configuration
-require('neodev').setup()
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Setup mason so it can manage external tooling
-require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
+      diagnostics = {
+          globals = { 'vim' },
+      }
     }
-  end,
+  }
 }
+
 
 -- ==================================================================== --
 --                          CMP Configuration                           --
--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ brew install font-fira-code-nerd-font
 
 brew install ripgrep #Telescope dependency
 
+# Install Language Servers
+brew install npm
+npm i -g typescript typescript-language-server # TypeScript / JS
+npm i -g vscode-langservers-extracted # Html, Css
+npm i -g bash-language-server # Bash
+
 # Initial Setup file for new systems
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
